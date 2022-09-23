eab171b76f82f951ac749acb754122201609e675
jake watters <jerkey@g-cipher.net>
2022-09-23 02:39:10 -0700

    print filename onto picture if successful

--- websnap.js
+++ websnap.js
@@ -3,6 +3,7 @@ let video = document.querySelector("#video");
 let take_photo = document.querySelector("#take-photo");
 let save_photo = document.querySelector("#save-photo");
 let canvas = document.querySelector("#canvas");
+let picture = canvas.getContext('2d') // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 
 const urlParams = new URLSearchParams(window.location.search);
 let photoId = urlParams.get('id')
@@ -19,20 +20,31 @@ document.addEventListener("DOMContentLoaded", async function() {
 });
 
 take_photo.addEventListener('click', function() {
-   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-});
+  picture.drawImage(video, 0, 0, canvas.width, canvas.height);
+}); // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
 
 save_photo.addEventListener('click', function() {
 canvas.toBlob(function(blob) {
-      file = new File([blob], photoId+'.jpg', { type: 'image/jpeg' });
+      let fileName = photoId+'.jpg'
+      file = new File([blob], fileName, { type: 'image/jpeg' });
       let data = new FormData();
 
       data.append('file', file);
 
       let request = new XMLHttpRequest();
-      request.open('POST', 'uploadpic'); 
-      request.addEventListener('load', function(e) {
-	      console.log(request.response);
+      request.open('POST', 'uploadpic'); // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+      request.addEventListener('loadend', function(e) {
+	      console.log("res:",request.response);
+              if (request.response == "ok") { // this only happens if the server did res.status(200).send('ok');
+                picture.font = "75px serif";
+                picture.fillStyle = '#FFFFFF';
+                picture.fillText("image saved as:", 5, 100);
+                picture.fillText(fileName, 5, 200);
+              } else {
+                picture.font = "75px serif";
+                picture.fillStyle = '#FF0000';
+                picture.fillText("ERROR! See console.log", 5, 100);
+              }
       });
       request.send(data);
   }, 'image/jpeg');
